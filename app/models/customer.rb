1e9525ab7cdb0330713e85f983b257d4f825734e
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-05-03 06:10:07 -0600

    Create models with relationships, validations and enums

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,12 @@
 class Customer < ApplicationRecord
+
+  validates :f_name, presence: true
+  validates :l_name, presence: true
+  validates :email, presence: true
+  validates :address, presence: true
+
+  has_secure_password
+
+  has_many :customer_subscriptions
+  has_many :subscriptions, through: :customer_subscriptions
 end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,2 +1,10 @@
 class Subscription < ApplicationRecord
+
+  validates :title, presence: true
+  validates :price, presence: true
+  validates :status, presence: true
+  validates :frequency, presence: true
+
+  has_many :customer_subscriptions
+  has_many :customers, through: :customer_subscriptions
 end
