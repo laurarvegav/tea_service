8e876d83479d91bd47a7299674d96a5f8f049a0c
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-05-03 06:09:45 -0600

    Create tests for model relationships, validations and enums

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:f_name) }
+    it { should validate_presence_of(:l_name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:address) }
+  end
+
+  describe 'relationships' do
+    it { should have_many :customer_subscriptions }
+    it { should have_many(:subscriptions).through(:customer_subscriptions) }
+  end
+
+  describe 'instance methods' do
+  end
 end
--- spec/models/customer_subscription_spec.rb
+++ spec/models/customer_subscription_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe CustomerSubscription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should belong_to :customer }
+    it { should belong_to :subscription }
+  end
+
+  describe 'Enums' do
+    it 'enums tests' do
+      should define_enum_for(:status).with_values(["active", "cancelled"])
+    end
+  end
+
+  describe 'instance methods' do
+  end
 end
