9986fa111461380ebcfc483904f56ecbae6cd55d
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-05-04 11:45:32 -0600

    Update subscription table to remove status column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_01_200149) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_04_173333) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,7 +37,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_01_200149) do
   create_table "subscriptions", force: :cascade do |t|
     t.string "title"
     t.float "price"
-    t.string "status"
     t.string "frequency"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
