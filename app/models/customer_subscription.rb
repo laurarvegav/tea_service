6097167516e22647394031695481728258e9846a
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-05-04 11:44:59 -0600

    Create validation for uniqueness of customer subsctiprion

--- app/models/customer_subscription.rb
+++ app/models/customer_subscription.rb
@@ -3,4 +3,5 @@ class CustomerSubscription < ApplicationRecord
   belongs_to :subscription
 
   enum status: ["active", "cancelled"]
+  validates :customer_id, uniqueness: { scope: :subscription_id }
 end
