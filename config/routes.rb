f995acc7a488f654a81d529aad81899b796dfc09
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-05-04 16:23:42 -0600

    Set up routes with index an ddestroy actions as well

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v0 do
       resources :customers do
-        resources :subscriptions, only: :create
+        resources :subscriptions, only: [:create, :index, :destroy]
       end
     end
   end
