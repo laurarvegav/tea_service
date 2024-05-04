98472b525c28287812b7461b64a06778cdd5c5a9
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-05-04 11:39:22 -0600

    Complete create action

--- app/controllers/api/v0/subscriptions_controller.rb
+++ app/controllers/api/v0/subscriptions_controller.rb
@@ -1,6 +1,11 @@
-class API::V0::SubscriptionsController < ApplicationController
+class Api::V0::SubscriptionsController < ApplicationController
 
   def create
-    require 'pry'; binding.pry
+    subscription = Subscription.find_by(title: params[:tea_service])
+    customer = Customer.find(params[:customer_id])
+
+    CustomerSubscription.create!({subscription: subscription, customer: customer})
+
+    render json: {message: "Subscription successfully added to Customer"}
   end
 end
